1cc5d62f2d3a8eb91669c040533fec890c381fba
wongchance <niaidewangchao@qq.com>
2017-07-26 14:20:57 +0800

    trello看板

---
+++ trello.NoCover.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         trllo no cover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       wongchance
+// @match        https://trello.com/b/LvwOjrYP/ingress-medal-arts
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+    jQuery(document).ready(function() {
+        setInterval(function() {
+            RemoveCover();
+        }, 100);
+
+    });
+
+    function RemoveCover() {
+        $('div.list-card-cover.js-card-cover').css('background-image', '').css('height', '0px');
+    }
+})();
