9e981432905a6a5dfece16dbf886d222342b5aba
wongchance <niaidewangchao@qq.com>
2018-01-16 23:17:02 +0800

    0.0.4
    添加localstorage

--- misson-author.extend.user.js
+++ misson-author.extend.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mission Author Extension
 // @namespace    http://tampermonkey.net/
-// @version      0.0.3
+// @version      0.0.4
 // @description  try to take over the world!
 // @author       wongchance
 // @match        https://mission-author-dot-betaspike.appspot.com/*
@@ -15,60 +15,116 @@
     'use strict';
     jQuery(document).ready(function () {
 
-        var navbarHtml = '<ul class="nav navbar-nav navbar-login" style=" float: left !important;">' +
-            '<li class="dropdown">' +
-            '<a href="" class="dropdown-toggle" data-toggle="dropdown">' +
-            '<span class="glyphicon"></span>' +
-            'Extension <b class="caret"></b>' +
-            '</a>' +
-            '<ul class="dropdown-menu">' +
-            '<li><a id="alllist" target="_self"><span class="glyphicon"></span> all</a></li>' +
-            '<li><a id="newDraftlist" target="_self"><span class="glyphicon"></span> new Draft</a></li>' +
-            '<li><a id="editDraftlist" target="_self"><span class="glyphicon"></span> edit Draft</a></li>' +
-            '<li><a id="newReviewlist" target="_self"><span class="glyphicon"></span> in New Review</a></li>' +
-            '<li><a id="editReviewlist" target="_self"><span class="glyphicon"></span> in Edit Review</a></li>' +
-            '<li><a id="publishedlist" target="_self"><span class="glyphicon"></span> published</a></li>' +
-            '</ul>' +
-            '</li>' +
-            '</ul>';
+        // var url = window.location.href;
+        // if (url.indexOf('edit') > -1) {
 
-        var publishedCount = 0;
-        // var rowsCount = $('div.missions-list').find('div.row.mission');
-        // for (var i = 0; i < rowsCount.length; i++) {
-        //     if ($(rowsCount[i]).attr('ng-class') == "'mission-list-item-submitted_and_published'" ||
-        //         $(rowsCount[i]).attr('ng-class') == "'mission-list-item-published'" ||
-        //         $(rowsCount[i]).attr('ng-class') == "'mission-list-item-draft_of_published_mission'") {
-        //         publishedCount = publishedCount + 1;
-        //     }
-        // }
-        var navbarCountHtml = '<ul class="nav navbar-nav navbar-login" style=" float: left !important;">' +
-            '<li class="dropdown">' +
-            '<a href="" id="publishedCount" class="dropdown-toggle" data-toggle="dropdown">' +
-            'published Count: ' + publishedCount +
-            '</a>' +
-            '</li>' +
-            '</ul>';
-
-        jQuery('div.navbar-collapse').append(navbarHtml);
-        jQuery('div.navbar-collapse').append(navbarCountHtml);
-
-        jQuery('#alllist')[0].addEventListener('click', showAlllist);
-        jQuery('#newDraftlist')[0].addEventListener('click', showNewDraftlist);
-        jQuery('#editDraftlist')[0].addEventListener('click', showEditDraftlist);
-
-        jQuery('#newReviewlist')[0].addEventListener('click', showNewReviewlist);
-        jQuery('#editReviewlist')[0].addEventListener('click', showEditReviewlist);
-        jQuery('#publishedlist')[0].addEventListener('click', showPublishedlist);
-
-
-
-        var t1 = setInterval(function () {
-            if ($('button.create-mission-button').length > 0) {
-                showAlllist();
-                clearInterval(t1);
+        var t0 = setInterval(function () {
+            if (jQuery('input[ng-model="mission.definition.name"]').length > 0 && jQuery('#saveStoragebtn').length < 1) {
+                var storageButton = '<div class="form-group"><label class="button" id="saveStoragebtn">save</label> <label class="button" id="loadStoragebtn">load</label></div>';
+                jQuery('input[ng-model="mission.definition.name"]').parent().parent().append(storageButton);
+                jQuery('#saveStoragebtn')[0].addEventListener('click', saveStorage);
+                jQuery('#loadStoragebtn')[0].addEventListener('click', loadStorage);
+                //clearInterval(t0);
             }
+
+
+
+            if ($('button.create-mission-button').length > 0 && jQuery('#alllist').length < 1) {
+                var navbarHtml = '<ul class="nav navbar-nav navbar-login" style=" float: left !important;">' +
+                    '<li class="dropdown">' +
+                    '<a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">' +
+                    '<span class="glyphicon"></span>' +
+                    'Extension <b class="caret"></b>' +
+                    '</a>' +
+                    '<ul class="dropdown-menu">' +
+                    '<li><a id="alllist" target="_self"><span class="glyphicon"></span> all</a></li>' +
+                    '<li><a id="newDraftlist" target="_self"><span class="glyphicon"></span> new Draft</a></li>' +
+                    '<li><a id="editDraftlist" target="_self"><span class="glyphicon"></span> edit Draft</a></li>' +
+                    '<li><a id="newReviewlist" target="_self"><span class="glyphicon"></span> in New Review</a></li>' +
+                    '<li><a id="editReviewlist" target="_self"><span class="glyphicon"></span> in Edit Review</a></li>' +
+                    '<li><a id="publishedlist" target="_self"><span class="glyphicon"></span> published</a></li>' +
+                    '</ul>' +
+                    '</li>' +
+                    '</ul>';
+
+                var publishedCount = 0;
+                // var rowsCount = $('div.missions-list').find('div.row.mission');
+                // for (var i = 0; i < rowsCount.length; i++) {
+                //     if ($(rowsCount[i]).attr('ng-class') == "'mission-list-item-submitted_and_published'" ||
+                //         $(rowsCount[i]).attr('ng-class') == "'mission-list-item-published'" ||
+                //         $(rowsCount[i]).attr('ng-class') == "'mission-list-item-draft_of_published_mission'") {
+                //         publishedCount = publishedCount + 1;
+                //     }
+                // }
+                var navbarCountHtml = '<ul class="nav navbar-nav navbar-login" style=" float: left !important;">' +
+                    '<li class="dropdown">' +
+                    '<a href="javascript:void(0)" id="publishedCount" class="dropdown-toggle" data-toggle="dropdown">' +
+                    'published Count: ' + publishedCount +
+                    '</a>' +
+                    '</li>' +
+                    '</ul>';
+
+                jQuery('div.navbar-collapse').append(navbarHtml);
+                jQuery('div.navbar-collapse').append(navbarCountHtml);
+
+                jQuery('#alllist')[0].addEventListener('click', showAlllist);
+                jQuery('#newDraftlist')[0].addEventListener('click', showNewDraftlist);
+                jQuery('#editDraftlist')[0].addEventListener('click', showEditDraftlist);
+
+                jQuery('#newReviewlist')[0].addEventListener('click', showNewReviewlist);
+                jQuery('#editReviewlist')[0].addEventListener('click', showEditReviewlist);
+                jQuery('#publishedlist')[0].addEventListener('click', showPublishedlist);
+                showAlllist();
+                //clearInterval(t1);
+            }
+
         }, 1000);
+
+        // }
+        // else {
+        //     var t1 = setInterval(function () {
+
+        //     }, 1000);
+        // }
     });
+
+
+    function loadStorage(event) {
+        var storage = window.localStorage;
+        if (!storage["name"] || !storage["description"]) {
+            if (!storage["name"] && !storage["description"]) {
+                if (confirm("name and description are both empty,still load?")) {
+                    jQuery('input[ng-model="mission.definition.name"]').val(storage["name"]);
+                    jQuery('textarea[ng-model="mission.definition.description"]').val(storage["description"]);
+
+                }
+            } else if (!storage["name"]) {
+                if (confirm("name is empty,still load?")) {
+                    jQuery('input[ng-model="mission.definition.name"]').val(storage["name"]);
+                    jQuery('textarea[ng-model="mission.definition.description"]').val(storage["description"]);
+
+                }
+            } else if (!storage["description"]) {
+                if (confirm("description is empty,still load?")) {
+                    jQuery('input[ng-model="mission.definition.name"]').val(storage["name"]);
+                    jQuery('textarea[ng-model="mission.definition.description"]').val(storage["description"]);
+                }
+            }
+        }
+        else {
+            jQuery('input[ng-model="mission.definition.name"]').val(storage["name"]);
+            jQuery('textarea[ng-model="mission.definition.description"]').val(storage["description"]);
+        }
+
+
+    }
+
+    function saveStorage(event) {
+        var storage = window.localStorage;
+        storage["name"] = jQuery('input[ng-model="mission.definition.name"]').val();
+        storage["description"] = jQuery('textarea[ng-model="mission.definition.description"]').val();
+    }
+
 
     function showAlllist(event) {
         var rows = $('div.missions-list').find('div.row.mission');
@@ -90,7 +146,6 @@
     function showNewReviewlist(event) {
         var rows = $('div.missions-list').find('div.row.mission');
         rows = resort(rows);
-
         for (var i = 0; i < rows.length; i++) {
             if ($(rows[i]).attr('ng-class') == "'mission-list-item-submitted'") {
                 $(rows[i]).show();
@@ -103,7 +158,6 @@
     function showEditReviewlist(event) {
         var rows = $('div.missions-list').find('div.row.mission');
         rows = resort(rows);
-
         for (var i = 0; i < rows.length; i++) {
             if ($(rows[i]).attr('ng-class') == "'mission-list-item-submitted_and_published'") {
                 $(rows[i]).show();
@@ -116,7 +170,6 @@
     function showPublishedlist(event) {
         var rows = $('div.missions-list').find('div.row.mission');
         rows = resort(rows);
-
         for (var i = 0; i < rows.length; i++) {
             if ($(rows[i]).attr('ng-class') == "'mission-list-item-published'") {
                 $(rows[i]).show();
@@ -130,7 +183,6 @@
     function showNewDraftlist(event) {
         var rows = $('div.missions-list').find('div.row.mission');
         rows = resort(rows);
-
         for (var i = 0; i < rows.length; i++) {
             if ($(rows[i]).attr('ng-class') == "'mission-list-item-draft'") {
                 $(rows[i]).show();
@@ -139,7 +191,6 @@
             }
         }
     }
-
 
 
     function showEditDraftlist(event) {
@@ -165,13 +216,19 @@
         return newrows;
     }
 
-    // var rows = $('div.missions-list').find('div.row.mission'); 
-    // rows = rows.sort(function (a, b) { //利用数组的排序方法重新排序对象
-    //     return $(a).find('span.name')[0].innerText.trim().localeCompare($(b).find('span.name')[0].innerText.trim());  //从大到小
-    // });
-    // $('.missions-list').empty();
+
+    // var rows = $('div.missions-list').find('div.row.mission');
     // for (var i = 0; i < rows.length; i++) {
-    //     $('div.missions-list').append($(rows[i]));
+    //     var name = $(rows[i]).find('span.name')[0].innerText.trim();
+    //     name=name.replace('0','');
+    //     console.log(name);
+    //     var match = name.match(/[0-9０-９①-⑱\u2460-\u2473\u3251-\u32bf]+/);
+    //     match[0] = match[0].replace(/[①-⑱]/g, function (n) { return n.charCodeAt(0) - "①".charCodeAt(0) + 1 });
+    //     match[0] = match[0].replace(/[０-９]/g, function (n) { return n.charCodeAt(0) - "０".charCodeAt(0) });
+    //     match[0] = match[0].replace(/[\u2460-\u2473]/g, function (n) { return n.charCodeAt(0) - "①".charCodeAt(0) + 1 });
+    //     match[0] = match[0].replace(/[\u3251-\u32bf]/g, function (n) { return n.charCodeAt(0) - "㉑".charCodeAt(0) + 21 });
+    //     console.log(match[0]);
+
     // }
 
 })();
