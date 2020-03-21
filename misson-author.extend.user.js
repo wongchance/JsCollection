f995db5042df6d8ee618cb7dc49a2e53c6abc9f6
wangchao <wangchao@zjshenyue.cn>
2020-03-21 13:06:29 +0800

    0.1.1

--- misson-author.extend.user.js
+++ misson-author.extend.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mission Author Extension
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       wongchance
 // @match        https://mission-author-dot-betaspike.appspot.com/*
